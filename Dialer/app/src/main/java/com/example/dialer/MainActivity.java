285428a1c44f60f6701ce1c69f5d478287c429a6
Eleandro Laureano <eleandrog@ua.pt>
2020-10-16 22:37:07 +0100

    HW1 - Incompleto

---
+++ Dialer/app/src/main/java/com/example/dialer/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.dialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonOne;
+    Button buttonTwo;
+    Button buttonThree;
+    Button buttonFour;
+    Button buttonFive;
+    Button buttonSix;
+    Button buttonSeven;
+    Button buttonEight;
+    Button buttonNine;
+    Button buttonStar;
+    Button buttonZero;
+    Button buttonHash;
+    Button buttonCall;
+    Button buttonDelete;
+
+    EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonOne = findViewById(R.id.buttonOne);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonFour = findViewById(R.id.buttonFour);
+        buttonFive = findViewById(R.id.buttonFive);
+        buttonSix = findViewById(R.id.buttonSix);
+        buttonSeven = findViewById(R.id.buttonSeven);
+        buttonEight = findViewById(R.id.buttonEight);
+        buttonNine = findViewById(R.id.buttonNine);
+        buttonStar = findViewById(R.id.buttonStar);
+        buttonZero = findViewById(R.id.buttonZero);
+        buttonHash = findViewById(R.id.buttonHash);
+        buttonDelete = findViewById(R.id.buttonDelete);
+        buttonCall = findViewById(R.id.buttonCall);
+
+        input = findViewById(R.id.editText);
+    }
+
+    public void one(View view) {
+
+    }
+
+    public void two(View view) {
+
+    }
+
+    public void three(View view) {
+
+    }
+
+    public void four(View view) {
+
+    }
+
+    public void five(View view) {
+
+    }
+
+    public void six(View view) {
+
+    }
+
+    public void seven(View view) {
+
+    }
+
+    public void eight(View view) {
+
+    }
+
+    public void nine(View view) {
+
+    }
+
+    public void star(View view) {
+
+    }
+
+    public void zero(View view) {
+
+    }
+
+    public void hash(View view) {
+
+    }
+
+    public void onDelete(View view) {
+
+    }
+
+    public void onCall(View view) {
+
+    }
+
+    public void onClickButton(Button button, EditText inputNumber, String number) {         //Um método para usar todos os onClicks ao invés de repetir código e usar um para cada um
+        String text = input.getText().toString();                                           //text convertido para string
+        inputNumber.setText(text + number);
+    }
+}
